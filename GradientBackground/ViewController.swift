99e1b21b21f543d90175b7784b10d7db03056f50
fiona <fi.carty@gmail.com>
2016-12-08 17:27:36 -0800

    added gradient background one view application

--- GradientBackground/ViewController.swift
+++ GradientBackground/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // top : rgb(172,201,255)
+        let topColor = UIColor(red: 172/255.0, green: 201/255.0, blue:  255/255.0, alpha: 1.0)
+        // bottom : rgb(255,215,190)
+        let bottomColor = UIColor(red: 255/255.0, green: 215/255.0, blue: 190/255.0, alpha: 1.0)
+        
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 100)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
